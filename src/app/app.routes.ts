401145d0af580b0a0efac3538bffe336ddf96e7f
abrar19 <abm.abrar@gmail.com>
2024-07-23 06:24:33 +0200

    working on login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'budget-planner',
+        path: '',
         loadChildren: () => import('./budget-planner/budget-planner.module').then( m => m.BudgetPlannerModule)
     }
 ];
--- src/app/budget-planner/login/login.component.ts
+++ src/app/budget-planner/login/login.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: any;
+  registerForm: any;
+  activeForm: 'login' | 'register' = 'login';
 
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    //Login Form
+    this.loginForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      }
+    )
+
+    //Registration Form
+
+    this.registerForm = this.fb.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      }
+    )
+  }
+
+  toggleForm(form: 'login' | 'register'){
+    this.activeForm = form;
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      console.log("Login Info====>>", this.loginForm.value);
+    }
+  }
 }
