09b9640035c1a245fb136d62be345448893903fd
abrar19 <abm.abrar@gmail.com>
2024-07-26 00:44:40 +0200

    done with one card

--- src/app/budget-planner/dashboard/dashboard.component.ts
+++ src/app/budget-planner/dashboard/dashboard.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { SideNavComponent } from '../side-nav/side-nav.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SideNavComponent],
+  imports: [SideNavComponent, MatIconModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  lastMonthsIncome = ['January: $2000', 'February: $1500', 'March: $1800'];
+  currentMonthIncome = '$2000'
 
+  onIncome(){
+    
+  }
 }
